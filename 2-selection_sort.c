596c66c11879323148345b0671c3b110ceeac0df
b7sh <mohammedabdelbasit500@gmail.com>
2024-01-20 04:15:59 -0800

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap - swap two elements in the array
+ * @num_1: the first element
+ * @num_@: the second element
+*/
+
+void swap(int *num_1, int *num_2)
+{
+	int temp = *num_1;
+	*num_1 = *num_2;
+	*num_2 = temp;
+}
+
+/**
+ * selection_sort - function to sort the array using
+ * selection sort alogrithm
+ * @array: hold the array elements
+ * @size: the size of the array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index_1, index_2;
+	size_t minimum;
+
+	for (index_1 = 0; index_1 < (size - 1); index_1 ++)
+	{
+		minimum = index_1;
+
+		for (index_2 = index_1 + 1; index_2 < size; index_2++)
+		{
+			if (array[index_2] < array[minimum])
+			{
+				minimum = index_2;
+			}
+		}
+		if (minimum != index_1)
+		{
+			swap(&array[index_1], &array[minimum]);
+			print_array(array, size);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void swap(int *num_1, int *num_2);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
